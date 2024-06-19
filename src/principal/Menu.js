288cee72764d3d772f68f1978a76d539b4b368e9
Eldavi5 <davidsilvabello25@gmail.com>
2024-06-19 17:07:04 -0600

    estilos nuevos

--- src/principal/Menu.js
+++ src/principal/Menu.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Parallax } from 'react-parallax'
 import { Header, About, Projects, Contact, Footer } from "../components"
 import "./Export.scss"
 
 export function Menu() {
     return (
         <div className="App">
-            <Parallax bgImage={require("../images/fondo.jpg")} strength={500}>
-                <div style={{ minHeight: '100vh' }}>
-                    <Header />
-                    <About />
-                    <Projects />
-                    <Contact />
-                    <Footer />
-                </div>
-            </Parallax>
+            <div style={{ minHeight: '100vh' }}>
+                <Header />
+                <About />
+                <Projects />
+                <Contact />
+                <Footer />
+            </div>
         </div>
     )
 }
