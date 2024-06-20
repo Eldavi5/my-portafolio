802bab1c37db3459c711eb23f07067eb7721bc44
Eldavi5 <davidsilvabello25@gmail.com>
2024-06-20 12:09:45 -0600

    Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import Logo from '../images/Logo.png';
 import '../styles/Header.scss';
 
 export const Header = () => (
     <Navbar expand="lg" className="custom-header">
         <Container>
             <Navbar.Brand className="custom-brand">
-                <h1>David Silva Bello</h1>
-                <p>Desarrollador Front-End</p>
+                <img src={Logo} alt="Logo" className="logo" />
+                <div className="brand-text">
+                    <h1>David Silva Bello</h1>
+                    <p>Desarrollador Front-End</p>
+                </div>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
